dfcecf575bd3c70da91bd57fa9b9826643303d93
Vyacheslav <IIec1337@yandex.ru>
2024-01-05 20:09:29 +0300

    change orm to raw sql queries

--- main/views.py
+++ main/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render
+from django.db import connection
 from .models import Doctors, Articles
+from collections import namedtuple
 
 # Create your views here.
 
 
 def index(request):
-    doctors = Doctors.objects.all()
-    news = Articles.objects.all().order_by('-date')
+    with connection.cursor() as cursor:
+        cursor.callproc('get_doctors_list')
+        doctors = namedTupleFetchAll(cursor)
+        cursor.callproc('get_news')
+        news = namedTupleFetchAll(cursor)
     return render(request, 'main/index.html', {'doctors': doctors, 'news': news})
 
 
 def about(request):
     return render(request, 'main/about.html')
+
+
+def namedTupleFetchAll(cursor):
+    """Return all rows from a cursor as a namedtuple"""
+    desc = cursor.description
+    nt_result = namedtuple('Result', [col[0] for col in desc])
+    return [nt_result(*row) for row in cursor.fetchall()]
