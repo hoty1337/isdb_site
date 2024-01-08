e2853701fb1a3168f02dc82ed654b900c4fdba07
Vyacheslav <IIec1337@yandex.ru>
2024-01-08 19:44:05 +0300

    models not needed anymore

--- main/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Doctors, ContactDetails, People, Articles
-
-
-admin.site.register(Doctors)
-admin.site.register(ContactDetails)
-admin.site.register(People)
-admin.site.register(Articles)
